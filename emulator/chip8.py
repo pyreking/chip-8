89b9a1b49793e47f43cb51b195c893cc076de0cb
Austin Guiney <austin.guiney@gmail.com>
2023-10-06 14:19:01 -0400

    Added support for resizing the screen.

--- emulator/chip8.py
+++ emulator/chip8.py
@@ -8,20 +8,26 @@ import tkinter as tk
 class Chip8:
     FPS = 60
 
-    def __init__(self, scale = 1):
-        self.window = tk.Tk()
-        self.screen = sc.Screen(self.window, 15)
-        self.keypad = kp.KeyPad(self.window)
-        self.speaker = sp.Speaker("../sound/beep.wav")
-        self.comp = cpu.CPU(self.screen, self.keypad, self.speaker)
+    def __init__(self, scale = 18):
+        self.root = tk.Tk()
+        root_frame = tk.Frame(self.root)
+        root_frame.pack(fill=tk.BOTH, expand=tk.YES)
+        screen = sc.Screen(root_frame, width = 64 * scale, height = 32 * scale,
+                           bg="white", highlightthickness=0)
+        screen.pack(fill=tk.BOTH, expand=tk.YES)
+        
+        keypad = kp.KeyPad(self.root)
+        speaker = sp.Speaker("../sound/beep.wav")
+
+        self.comp = cpu.CPU(screen, keypad, speaker)
         self.comp.load_sprites_into_memory()
         self.comp.load_rom("../roms/PONG")
         self.step()
-        self.window.mainloop()
+        self.root.mainloop()
 
     def step(self):
         self.comp.cycle()
-        self.window.after(int(1000 / 60), self.step)
+        self.root.after(int(1000 / 60), self.step)
 
 if __name__ == "__main__":
     emulator = Chip8()
