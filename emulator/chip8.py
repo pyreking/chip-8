6dbb88ac5f01aef95297ae2ec0539d9f3ee36e17
Austin Guiney <austin.guiney@gmail.com>
2023-10-06 14:59:36 -0400

    Refactored main program to subclass a tkinter frame.

--- emulator/chip8.py
+++ emulator/chip8.py
@@ -5,29 +5,32 @@ import speaker as sp
 import tkinter as tk
 
 
-class Chip8:
+class Chip8(tk.Frame):
     FPS = 60
 
-    def __init__(self, scale = 18):
-        self.root = tk.Tk()
-        root_frame = tk.Frame(self.root)
-        root_frame.pack(fill=tk.BOTH, expand=tk.YES)
-        screen = sc.Screen(root_frame, width = 64 * scale, height = 32 * scale,
+    def __init__(self, parent, scale, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+
+        screen = sc.Screen(self, width = 64 * scale, height = 32 * scale,
                            bg="white", highlightthickness=0)
+        
         screen.pack(fill=tk.BOTH, expand=tk.YES)
         
-        keypad = kp.KeyPad(self.root)
+        keypad = kp.KeyPad(parent)
         speaker = sp.Speaker("../sound/beep.wav")
 
         self.comp = cpu.CPU(screen, keypad, speaker)
+
         self.comp.load_sprites_into_memory()
         self.comp.load_rom("../roms/PONG")
         self.step()
-        self.root.mainloop()
 
     def step(self):
         self.comp.cycle()
-        self.root.after(int(1000 / 60), self.step)
+        self.after(int(1000 / Chip8.FPS), self.step)
 
 if __name__ == "__main__":
-    emulator = Chip8()
+    root = tk.Tk()
+    emulator = Chip8(root, 18)
+    emulator.pack(side="top", fill="both", expand=True)
+    root.mainloop()
