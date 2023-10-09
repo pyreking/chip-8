c2ec3d3aabf59ed0f598729bcd22cbc42659c817
Austin Guiney <austin.guiney@gmail.com>
2023-10-09 15:44:28 -0400

    Added pause and exit options.

--- emulator/chip8_menu.py
+++ emulator/chip8_menu.py
@@ -1,15 +1,25 @@
 import tkinter as tk
 from tkinter import filedialog
 
+class Options:
+    OPEN = 0
+    PAUSE = 1
+    EXIT = 2
+
 class Chip8Menu(tk.Menu):
+    PAUSE_LABELS = ["Pause", "Unpause"]
+
     def __init__(self, parent, cpu, step):
-        tk.Menu.__init__(self, parent, tearoff=False)
+        tk.Menu.__init__(self, parent, tearoff = False)
+        self.parent = parent
         self.cpu = cpu
         self.step = step
 
-        file_menu = tk.Menu(self, tearoff=0)
-        self.add_cascade(label="File", menu=file_menu)
-        file_menu.add_command(label="Open", command=self.on_open)
+        self.file_menu = tk.Menu(self, tearoff = False)
+        self.add_cascade(label="File", menu=self.file_menu)
+        self.file_menu.add_command(label="Open", command=self.on_open)
+        self.file_menu.add_command(label="Pause", command=self.on_pause, state="disabled")
+        self.file_menu.add_command(label="Exit", command=self.on_exit)
 
         parent.config(menu=self)
     
@@ -22,10 +32,19 @@ class Chip8Menu(tk.Menu):
 
         if filename != '':
             self.cpu.load_rom(filename)
-            self.cpu.paused = False
-            self.step()
-        
+            self.file_menu.entryconfigure(Options.PAUSE, state="active", label=self.PAUSE_LABELS[0])
+
         self.cpu.paused = False
+        self.step()
+    
+    def on_pause(self):
+        self.cpu.paused ^= 1
+        idx = self.cpu.paused
+        self.file_menu.entryconfig(Options.PAUSE, label=self.PAUSE_LABELS[idx])
+        self.step()
+    
+    def on_exit(self):
+        self.parent.destroy()
     
 if __name__ == '__main__':
     root = tk.Tk()
