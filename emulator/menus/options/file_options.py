1f851d814534e271d47727c37e4d60e5f203cd1b
Austin Guiney <austin.guiney@gmail.com>
2023-11-13 18:58:28 -0500

    - Created seperate files for FileOptions and GameOptions. - Fixed a bug involving mislabeled options in FileOptions.

---
+++ emulator/menus/options/file_options.py
@@ -0,0 +1,9 @@
+class FileOptions:
+    """A list of options for the menu bar.
+
+    An enum class that reperesents the options availiable for the menu bar.
+    """
+    OPEN = 0
+    SAVE = 1
+    LOAD = 2
+    EXIT = 3
---
+++ emulator/menus/options/game_options.py
@@ -0,0 +1,6 @@
+class GameOptions:
+    # The labels for the pause option.
+    PAUSE_LABELS = ["Pause", "Unpause"]
+    PAUSE = 0
+    REWIND = 1
+    FAST_FORWARD = 2
