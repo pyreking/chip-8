5c76647784d0443e42ba44f1b531f5e6d61cacca
Austin Guiney <austin.guiney@gmail.com>
2024-02-05 13:21:11 -0500

    Updated the documentation for menus/options/file_options.py.

--- emulator/menus/options/file_options.py
+++ emulator/menus/options/file_options.py
@@ -1,7 +1,13 @@
-class FileOptions:
-    """A list of options for the menu bar.
+"""
+file_options.py:
 
-    An enum class that reperesents the options availiable for the menu bar.
+Create symbolic constants for the options in the file menu.
+"""
+
+class FileOptions:
+    """A list of options for the file menu.
+
+    An enum class that reperesents the options availiable for the file bar.
     """
     OPEN = 0
     SAVE = 1
