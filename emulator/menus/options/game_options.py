e946d104a6c3725f4365c9443d4b778f3379c59c
Austin Guiney <austin.guiney@gmail.com>
2024-02-05 13:21:42 -0500

    Updated the documentation for menus/options/game_options.py.

--- emulator/menus/options/game_options.py
+++ emulator/menus/options/game_options.py
@@ -1,4 +1,14 @@
+"""
+game_options.py:
+
+Create symbolic constants for the options in the game menu.
+"""
+
 class GameOptions:
+    """A list of options for the game menu.
+
+    An enum class that reperesents the options availiable for the game menu.
+    """
     # The labels for the pause option.
     PAUSE_LABELS = ["Pause", "Unpause"]
     PAUSE = 0
